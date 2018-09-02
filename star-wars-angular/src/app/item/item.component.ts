cc36d70f01ea01b7ede482f83154a9996c84647b
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-02 15:22:51 -0500

    update

--- star-wars-angular/src/app/item/item.component.ts
+++ star-wars-angular/src/app/item/item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
@@ -7,15 +9,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() character;
-  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
+  swService: StarWarsService;
 
-  constructor() { }
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
 
   ngOnInit() {
   }
 
   onAssign(side) {
     // this.character.side = side;
-    this.sideAssigned.emit({name: this.character.name, side: side});
+    // this.sideAssigned.emit({name: this.character.name, side: side});
+    this.swService.onSideChosen({ name: this.character.name, side: side });
   }
 }
