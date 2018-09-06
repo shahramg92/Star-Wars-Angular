c66aa724173280f009261b4671a5720e59777b62
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-06 17:20:09 -0500

    update

--- star-wars-angular/src/app/star-wars.service.ts
+++ star-wars-angular/src/app/star-wars.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { LogService } from './log.service';
 
@@ -9,6 +10,7 @@ export class StarWarsService {
     { name: 'Darth Vader', side: '' }
   ];
   private logService: LogService;
+  charactersChanged = new Subject<void>();
 
   constructor(logService: LogService) {
     this.logService = logService;
@@ -28,6 +30,7 @@ export class StarWarsService {
       return char.name === charInfo.name;
     })
     this.characters[pos].side = charInfo.side;
+    this.charactersChanged.next();
     this.logService.writeLog('Changed side of ' + charInfo.name + ', new side: ' + charInfo.side);
   }
 
